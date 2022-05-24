b8fcc927bcada5b9810ddf75cf3360b60e4beb61
najwanan <theunnamedrd@gmail.com>
2022-05-23 23:17:13 -0700

    adding psuedocode

--- FirstUniqueCharacterInAString/app.js
+++ FirstUniqueCharacterInAString/app.js
@@ -13,4 +13,19 @@ Output: 2
 Example 3:
 
 Input: s = "aabb"
-Output: -1*/
+Output: -1
+
+
+1. Understand the problem 
+ - take in a string s 
+ - check string for first non-repeating char and return the index if exists 
+ - else return -1 
+ 
+ 2.Devise a plan
+ - loop over the string and track in hashmap how many times char exists 
+ - loop over string and check hashmap for each char, if the value is > 1 we know it is repeating
+ - if the value is 1 return the index
+ - if all repeating char in s return -1 
+ 
+ 3. Carry out the plan 
+ 4. Look back*/
