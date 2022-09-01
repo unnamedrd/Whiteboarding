a3c5e1d4061dc49fb054d662ea020f9d5958ae2c
najwanan <theunnamedrd@gmail.com>
2022-08-31 22:15:09 -0700

    adding psuedo code for longest palindrome

---
+++ LongestPalindrome/app.js
@@ -0,0 +1,9 @@
+var longestPalindrome = function (s) {
+  //input is a string, no letters, no special characters
+  //case sensitve, can be lower or upper case
+  //no null
+  // an argument with a length of 1 results in a return of 1
+  //return an integer which represents longest palindrome
+  //how is a palindrome created, when you have multiples of two of a letter
+  //either a combination of an even number of pairs of letters, or an even number of pairs of letters with one odd pairing
+};
